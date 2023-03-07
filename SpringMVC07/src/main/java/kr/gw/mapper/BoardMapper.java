ea28cf89801d59f2c2643e6f14f1ddf1755a92d9
gwooden22 <gwooden22@gmail.com>
2023-03-07 16:25:44 +0900

    member 테이블 생성 및 junit 테스트

--- SpringMVC07/src/main/java/kr/gw/mapper/BoardMapper.java
+++ SpringMVC07/src/main/java/kr/gw/mapper/BoardMapper.java
@@ -1,7 +1,12 @@
 package kr.gw.mapper;
 
+import java.util.List;
+
+import kr.gw.entity.Board;
+
 public interface BoardMapper {
 	
+	public List<Board> getList();
 	
 
 }
--- SpringMVC07/target/classes/kr/gw/entity/db.sql
+++ SpringMVC07/target/classes/kr/gw/entity/db.sql
@@ -16,3 +16,49 @@ create table tblBoard(
 	boardAvailable int,
 	primary key(idx)
 );
+
+select * from tblBoard;
+
+
+select max(idx) from tblBoard; --null
+select IFNULL(max(idx)+1, 1) from tblBoard;
+select IFNULL(max(boardGroup)+1, 0) from tblBoard;
+
+insert into tblBoard
+select IFNULL(max(idx)+1, 1),'gw01','게시판제목','게시판내용','관리자',
+now(), 0, IFNULL(max(boardGroup)+1, 0), 0, 0, 1
+from tblBoard;
+
+
+insert into tblBoard
+select IFNULL(max(idx)+1, 1),'gw01','게시판제목','게시판내용','지우든',
+now(), 0, IFNULL(max(boardGroup)+1, 0), 0, 0, 1
+from tblBoard;
+
+
+insert into tblBoard
+select IFNULL(max(idx)+1, 1),'gw01','게시판제목','게시판내용','gwooden',
+now(), 0, IFNULL(max(boardGroup)+1, 0), 0, 0, 1
+from tblBoard;
+
+-- 테이블 member --
+create table tblMember(
+  memID varchar(50) not null, -- 회원ID
+  memPwd varchar(50) not null, -- 회원비번
+  memName varchar(50) not null, -- 회원이름
+  memPhone varchar(50) not null, -- 회원전화번호
+  memAddr varchar(100), -- 회원주소
+  latitude decimal(13,10), -- 현재위치위도
+  longitude decimal(13,10), -- 현재위치경도
+  primary key(memID)
+);
+
+select * from tblMember;
+
+insert into tblMember(memID, memPwd, memName, memPhone)
+values('bit01','bit01','관리자','010-1111-1111');
+insert into tblMember(memID, memPwd, memName, memPhone)
+values('bit02','bit02','박매일','010-2222-2222');
+insert into tblMember(memID, memPwd, memName, memPhone)
+values('bit03','bit03','홍길동','010-3333-3333');
+
